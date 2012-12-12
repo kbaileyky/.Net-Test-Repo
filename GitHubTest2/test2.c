67f48e5f80beb8040a2af932dce4fca512ad8646
Kendall Bailey <kbailey@trinity.edu>
2012-12-12 09:52:31 -0600

    test 2

---
+++ GitHubTest2/test2.c
@@ -0,0 +1,28 @@
+/*
+ * test2.c
+ *
+ *  Created on: Dec 12, 2012
+ *      Author: kbailey
+ */
+#include "test2.h"
+
+int main(){
+	Test1(1);
+	Test1(4);
+
+	Test2("qwer");
+	return 0;
+}
+
+void Test1(int num){
+	int i;
+	for(i = 0; i <= num; i++){
+		printf("%d ", i);
+
+	}
+	printf("\n");
+}
+
+void Test2(char *str){
+	printf("%s", str);
+}
